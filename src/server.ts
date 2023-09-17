79db4bc14eaf6e0b899c526cba230d5a271e4579
LucasMorais1998 <lucas14.morais@gmail.com>
2023-09-17 15:46:40 -0300

    config categories routes

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import { categories } from './routes/categories.routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/categories', categories);
 
 app.listen(3333, () => console.log('server is running!'));
