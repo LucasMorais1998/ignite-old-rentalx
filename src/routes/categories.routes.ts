b4c5541529a23a462ff81f4c583790d16a1a0f5a
LucasMorais1998 <lucas14.morais@gmail.com>
2023-09-19 23:29:18 -0300

    implements createCategory useCases

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,24 +1,18 @@
 import { Router } from 'express';
 import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
-import { CreateCategoryService } from '../modules/cars/services/CreateCategoryService';
+import { createCategoryController } from '../modules/cars/useCases/createCategory';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
-categoriesRoutes.post('/', (req, res) => {
-  const { name, description } = req.body;
-
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-  createCategoryService.execute({ name, description });
-
-  return res.status(201).send();
+categoriesRoutes.post('/', (request, response) => {
+  return createCategoryController.handle(request, response);
 });
 
-categoriesRoutes.get('/', (req, res) => {
+categoriesRoutes.get('/', (request, response) => {
   const allCategories = categoriesRepository.list();
 
-  return res.json(allCategories);
+  return response.json(allCategories);
 });
 
 export { categoriesRoutes };
