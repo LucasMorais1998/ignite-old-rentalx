09e1dfa4143fab61880efdda5fe9bf651a5e506c
Lucas Morais <lucas14.morais@gmail.com>
2024-03-30 23:39:43 -0300

    refactor: adapts container to clean arch

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,3 +1,9 @@
+import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
+import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
+import { CategoriesRepository } from '@modules/cars/infra/typeorm/repositories/CategoriesRepository';
+import { SpecificationRepository } from '@modules/cars/infra/typeorm/repositories/SpecificationsRepository';
+import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository';
+import { ISpecificationRepository } from '@modules/cars/repositories/ISpecificationsRepository';
 import { container } from 'tsyringe';
 import { IUsersRepository } from '../../modules/accounts/repositories/IUsersRepository';
 import { UsersRepository } from '../../modules/accounts/repositories/implementations/UsersRepository';
