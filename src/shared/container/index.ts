853dbafb57fda665e821b9fec710868c48c5aba9
Lucas Morais <lucas14.morais@gmail.com>
2024-03-31 19:25:54 -0300

    feat: implements CarsRepository in container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,7 +1,9 @@
 import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
+import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository';
 import { CategoriesRepository } from '@modules/cars/infra/typeorm/repositories/CategoriesRepository';
 import { SpecificationRepository } from '@modules/cars/infra/typeorm/repositories/SpecificationsRepository';
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
 import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository';
 import { ISpecificationRepository } from '@modules/cars/repositories/ISpecificationsRepository';
 import { container } from 'tsyringe';
@@ -25,4 +27,9 @@ container.registerSingleton<ISpecificationRepository>(
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository
+);
+
+container.registerSingleton<ICarsRepository>(
+  'CarsRepository',
+  CarsRepository
 );
