130c2b42c3ec3e9f3bc56dfe65b53c3f8a03e291
Lucas Morais <lucas14.morais@gmail.com>
2024-06-27 23:16:42 -0300

    feat: adds deleteCarImages to Cars routes

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,6 +1,7 @@
 import uploadConfig from '@config/upload';
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
 import { CreateCarSpecificationController } from '@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController';
+import { DeleteCarImagesController } from '@modules/cars/useCases/deleteCarImages/DeleteCarImagesController';
 import { ListAvailableCarsController } from '@modules/cars/useCases/listAvailableCars/ListAvailableCarsController';
 import { UploadCarImagesController } from '@modules/cars/useCases/UploadCarImages/UploadCarImagesController';
 import { Router } from 'express';
@@ -14,6 +15,7 @@ let createCarController = new CreateCarController();
 let listAvailableCarsController = new ListAvailableCarsController();
 let createCarSpecificationController = new CreateCarSpecificationController();
 let uploadCarImagesController = new UploadCarImagesController();
+let deleteCarImagesController = new DeleteCarImagesController();
 
 const uploadCarImages = multer(uploadConfig.upload('./tmp/cars'));
 
@@ -39,6 +41,13 @@ carsRoutes.post(
   ensureAdmin,
   uploadCarImages.array('images'),
   uploadCarImagesController.handle
+);
+
+carsRoutes.delete(
+  '/images',
+  ensureAuthenticated,
+  ensureAdmin,
+  deleteCarImagesController.handle
 );
 
 export { carsRoutes };
