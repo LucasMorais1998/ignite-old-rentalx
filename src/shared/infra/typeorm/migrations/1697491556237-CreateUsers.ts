fefcdcdf8e5dbc7813e2ceab0a1d742c170d0a62
Lucas Morais <lucas14.morais@gmail.com>
2024-07-07 23:32:17 -0300

    fix: adjusts table and remove unnecessary migration

--- src/shared/infra/typeorm/migrations/1697491556237-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1697491556237-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1697491556237 implements MigrationInterface {
           {
             name: 'id',
             type: 'uuid',
+            isPrimary: true,
           },
           {
             name: 'name',
