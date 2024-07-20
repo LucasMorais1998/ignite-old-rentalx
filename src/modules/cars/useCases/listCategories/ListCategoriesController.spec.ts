924abf0699fb8fa24eb1b878d04722ea9ef432c9
Lucas Morais <lucas14.morais@gmail.com>
2024-07-20 20:52:33 -0300

    test: refactors ListCategoriesController

--- src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
@@ -1,10 +1,9 @@
-import { Category } from '@modules/cars/infra/typeorm/entities/Category';
-import { AuthUtils } from '@shared/__tests__/utils/AuthUtils';
 import { DatabaseUtils } from '@shared/__tests__/utils/DatabaseUtils';
 import { app } from '@shared/infra/http/app';
 import createConnection from '@shared/infra/typeorm';
 import request from 'supertest';
 import { Connection } from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
 
 let connection: Connection;
 
@@ -14,52 +13,47 @@ describe('List Categories Controller', () => {
     await connection.runMigrations();
 
     await DatabaseUtils.createAdminUser(connection);
-  });
 
-  beforeEach(async () => {
-    await connection.query('DELETE FROM categories');
-  });
+    await DatabaseUtils.createCategory(
+      connection,
+      uuidV4(),
+      'Category Test 1',
+      'Category test description 1'
+    );
 
+    await DatabaseUtils.createCategory(
+      connection,
+      uuidV4(),
+      'Category Test 2',
+      'Category test description 2'
+    );
+
+    await DatabaseUtils.createCategory(
+      connection,
+      uuidV4(),
+      'Category Test 3',
+      'Category test description 3'
+    );
+  });
+  
   afterAll(async () => {
     await connection.dropDatabase();
     await connection.close();
   });
 
-  it('should be able to list all categories', async () => {
-    const token = await AuthUtils.authenticateAdmin();
+  it('should list all categories successfully', async () => {
+    const response = await request(app).get('/categories');
 
-    const newCategoryData = {
-      name: 'Category Test',
-      description: 'Category test description',
-    };
-
-    await request(app)
-      .post('/categories')
-      .send(newCategoryData)
-      .set({
-        authorization: `Bearer ${token}`,
-      });
-
-    const { body, status } = (await request(app).get('/categories')) as {
-      body: Category[];
-      status: any;
-    };
-
-    expect(status).toBe(200);
-    expect(body).toHaveLength(1);
-    expect(body[0]).toHaveProperty('id');
-    expect(body[0].name).toEqual('Category Test');
-    expect(body[0].description).toEqual('Category test description');
-    expect(body[0]).toHaveProperty('created_at');
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveLength(3);
   });
 
   it('should return an empty list if there are no categories', async () => {
-    const { body, status } = (await request(app).get('/categories')) as {
-      body: Category[];
-      status: any;
-    };
+    await connection.query('DELETE FROM categories');
 
-    expect(status).toBe(200);
-    expect(body).toHaveLength(0);
+    const response = await request(app).get('/categories');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveLength(0);
   });
 });
